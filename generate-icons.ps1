0aaff9bcdabcf8303e75df9e3961de24faeb1e80
Vincent Rémond <vincent.remond@gmail.com>
2023-01-12 16:20:48 +0100

    Initial commit

---
+++ generate-icons.ps1
@@ -0,0 +1,6 @@
+Set-Alias inkscape "C:\Program Files\Inkscape\bin\inkscape.exe"
+
+inkscape --export-filename src/icons/icon-16.png --export-width=16 --export-height=16 icon.svg
+inkscape --export-filename src/icons/icon-32.png --export-width=32 --export-height=32 icon.svg
+inkscape --export-filename src/icons/icon-48.png --export-width=48 --export-height=48 icon.svg
+inkscape --export-filename src/icons/icon-128.png --export-width=128 --export-height=128 icon.svg
